9c25d16bf23536458d314c5625e10b26dfceb6e8
naledimathebula <naledimathebula18@gmail.com>
2026-08-31 11:47:45 +0200

    404

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 export default function NotFound() {
   return (
     <div className="not-found-page">
-      <h1>404</h1>
+      <h1>ERROR 404</h1>
       <p>Sorry, we couldn't find that page.</p>
       <Link to="/" className="btn-primary-link">Go back home</Link>
     </div>
